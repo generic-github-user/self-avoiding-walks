fd08d33861cd2400fba8b74b9cfb6012ee65efbc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-27 11:49:15 -0400

    Create self-avoiding-walk.py

---
+++ self-avoiding-walk.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Refer to the [Wikipedia article](https://en.wikipedia.org/wiki/Self-avoiding_walk) on self-avoiding walks for a good primer on the subject.
+
+# In[32]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numba as nb
+import itertools
+import random
