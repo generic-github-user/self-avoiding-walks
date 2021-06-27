901fa9d78a08aa4d5aa5d01378300dd997fbabb0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-27 11:49:32 -0400

    generate list of possible steps

--- self-avoiding-walk.py
+++ self-avoiding-walk.py
@@ -11,3 +11,20 @@ import matplotlib.pyplot as plt
 import numba as nb
 import itertools
 import random
+
+
+# In[244]:
+
+
+dimensions = 2
+D = dimensions
+z = 10
+
+start = [0, 0]
+choices = []
+
+for n in range(dimensions):
+    for y in [-1, 1]:
+        delta = np.zeros(dimensions).astype(np.int)
+        delta[n] = y
+        choices.append(delta)
