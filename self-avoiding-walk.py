43ea396305b550f2ab09316fe612027e9e542caf
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-28 21:38:56 -0400

    Add option for randomizing tree search

--- self-avoiding-walk.py
+++ self-avoiding-walk.py
@@ -25,7 +25,7 @@
 #  - https://iopscience.iop.org/article/10.1088/0305-4470/38/42/001
 #  - https://mathoverflow.net/questions/67192/exactly-simulating-a-random-walk-from-infinity
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -35,7 +35,7 @@ import itertools
 import random
 
 
-# In[2]:
+# In[3]:
 
 
 dimensions = 2
@@ -55,7 +55,7 @@ choices = np.stack(choices)
 print(choices)
 
 
-# In[3]:
+# In[4]:
 
 
 steps = []
@@ -75,7 +75,7 @@ def valid_moves(g, m, q):
     return filtered
 
 
-# In[4]:
+# In[5]:
 
 
 @nb.jit(nopython=True)
@@ -93,11 +93,11 @@ def clip(x, a, b):
     return x
 
 
-# In[9]:
+# In[134]:
 
 
-#@nb.njit#(parallel=True)
-def simulate(z, m=1, backtrack=True):
+@nb.njit#(parallel=True)
+def simulate(z, m=1, backtrack=True, randomize=True):
     for x in range(1):
         pos = np.array([0, 0])
 #         grid = np.zeros([z] * D)
@@ -108,9 +108,14 @@ def simulate(z, m=1, backtrack=True):
 
 #         steps = np.zeros((z**2, 2))
 #         steps[0] = pos
-        level = 0
+        level = 0 # ?
 #         TODO: randomize initial branches
-        branches = np.zeros((z**2,), dtype=np.int64)
+
+        if randomize:
+#             np.random.shuffle(choices)
+            branches = np.random.randint(0, 3, (z**2,))
+        else:
+            branches = np.zeros((z**2,), dtype=np.int64)
         
 #         Loop through (n^2)*m steps, where n is the width of the grid and m is a coefficient
         for t in range(z**2*m):
@@ -118,35 +123,59 @@ def simulate(z, m=1, backtrack=True):
     #         print(grid[tuple(pos+delta[0])])
             possible = valid_moves(grid, choices, pos)
 #             print(possible)
-            grid[pos[0], pos[1]] = level+(z**2//4)
             
-            if len(possible) > 0 and branches[level] < len(possible):
+
+#             if branches[level] > len(possible):
+#                 branches[level] = len(possible) - 1
+    
+#             B = branches[level] < len(possible)
+#             B = branches[level] < len(choices)
+            B = True
+#             print(possible)
+            
+            grid[pos[0], pos[1]] = level+1#+(z**2//4)
+            if len(possible) > 0 and B:
 #                 delta = random.choice(possible)
 #                 delta = np.random.choice(possible)
 #                 np.random.shuffle(possible)
 #                 index = np.random.randint(0, len(possible))
 #                 branches[level] = index
 #                 delta = possible[index]
-                delta = possible[branches[level]]
 
+#                 if randomize:
+#                     random.shuffle(possible)
+
+                index = branches[level]
+                if index >= len(possible):
+                    index %= len(possible)
+#                 print(index)
+                delta = possible[index]
+                
+#                 grid[tuple(pos)] = 1
+#                 print(pos[0])
+
+                
 #                 steps.append(delta)
                 pos += delta
 #                 steps.append(delta)
 #                 steps[l] = delta
 
 #                 pos = np.clip(pos, 0, z-1)
-                
+
                 pos = clip(pos, 0, z)
-                
-#                 grid[tuple(pos)] = 1
-#                 print(pos[0])
 
 #                 Move to the next "level" below the current one
 #                 Only increase the step count if there are still spaces to move to
                 if np.count_nonzero(grid) <= z**2-1 and level < z**2-1:
                     level += 1
-                    branches[level] = 0
-            else:
+#                     if randomize:
+#                         branches[level] = np.random.randint(0, 4)
+#                     else:
+                    if not randomize:
+                        branches[level] = 0
+            
+
+            elif np.count_nonzero(grid) < z**2:
 #                 lengths.append(t)
 #                 walks.append(grid)
                 if backtrack:
@@ -154,25 +183,31 @@ def simulate(z, m=1, backtrack=True):
             
 #                     Reset value of current position and checked sub-branch
                     grid[pos[0], pos[1]] = 0
+#                     if not randomize:
                     branches[level] = 0
                     
                     pos -= delta
                     pos = clip(pos, 0, z)
+
 #                     steps.pop()
                     level -= 1
+                    if level < 0:
+                        level = 0
+#                     print(f'Backtracking to level {level} (position: {pos})')
+#                     print(level)
                     branches[level] += 1
                 else:
                     break
                 
-#                 End simulation early if a "perfect" path which covers the entire grid is found
-                if np.count_nonzero(grid) >= z**2-1:
-                    break
+        #                 End simulation early if a "perfect" path which covers the entire grid is found
+            if np.count_nonzero(grid) >= z**2:
+                break
 #         else:
 #         walks.append(grid)
     return grid
 
 
-# In[18]:
+# In[187]:
 
 
 # Store the best discovered path (i.e., the one that covers the most cells in the grid)
@@ -181,8 +216,8 @@ best = None
 lengths = []
 walks = []
 # Run multiple simulations
-for i in range(300):
-    G = simulate(5, 3, True)
+for i in range(20000):
+    G = simulate(8, 4, True, True)
 #     if best:
 #         print(best.max())
 #     lengths.append(G.max())
@@ -207,9 +242,13 @@ plt.axis('off')
 # avoid and/or break at 
 # add backtracking
 # add step labels
+# implement backbite algorithm for Hamiltonian path generation
+# progressive upscaling+mutation?
+# tracing/wrapping paths
+# estimate needed compute
 
 
-# In[262]:
+# In[120]:
 
 
 # Plot a histogram of the path lengths
